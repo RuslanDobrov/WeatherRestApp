813fbe5549ee64726fa17b8478eea26fee89735b
Ruslan Dobrov <dobrovr90@gmail.com>
2023-08-29 11:02:51 +0300

    first commit

---
+++ src/main/java/ruslan/dobrov/WeatherRestApp/WeatherRestAppApplication.java
@@ -0,0 +1,13 @@
+package ruslan.dobrov.WeatherRestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherRestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ruslan/dobrov/WeatherRestApp/WeatherRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ruslan.dobrov.WeatherRestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
